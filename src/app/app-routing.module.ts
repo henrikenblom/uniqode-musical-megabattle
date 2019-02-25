b5c3c717e02a6a16074d3b253ccbdcb5f4ef294e
Henrik Enblom <henrik@enblom.com>
2019-02-25 23:41:34 +0100

    Configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SignInComponent} from "./components/sign-in/sign-in.component";
+import {MusicQuizComponent} from "./components/music-quiz/music-quiz.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/sign-in', pathMatch: 'full'},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'music-quiz', component: MusicQuizComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { MusicQuizComponent } from './components/music-quiz/music-quiz.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
+import {MusicQuizComponent} from './components/music-quiz/music-quiz.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
@@ -28,4 +28,5 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
