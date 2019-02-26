6b5160142d8bb448341d4ab114db6dd0b5e65fd0
Henrik Enblom <henrik@enblom.com>
2019-02-26 20:06:58 +0100

    Logo/tagline fades in/out and gets removed after five seconds

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,17 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+
+      state('in', style({opacity: 1})),
+
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(600)
+      ]),
+
+      transition(':leave',
+        animate(600, style({opacity: 0})))
+    ])
+  ]
 })
 export class SignInComponent implements OnInit {
+
+  preLogin = true;
 
   constructor(public authService: AuthService) {
   }
 
   ngOnInit() {
+    setInterval(() => {
+      this.preLogin = false;
+    }, 5000);
   }
 
 }
